a7a8281102eac050e0c390030236ba09341ca6c4
aDyon4 <12adamdominik@gmail.com>
2025-01-28 16:02:11 +0100

    search, filter, sort by

--- src/pages/Etelek.jsx
+++ src/pages/Etelek.jsx
@@ -1,9 +1,41 @@
+import { Box, InputAdornment, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import React from 'react'
+import { IoSearch, IoFilter } from "react-icons/io5";
+import { BiSort } from "react-icons/bi";
+
 
 export default function Etelek() {
   return (
     <div className='etelek'>
-      Etelek
+      <div className="kereso">
+        <Box>
+          <TextField
+            className="searchBTN"
+            color="dark"
+            variant="standard"
+            placeholder='Search..'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IoSearch />
+                </InputAdornment>
+              ),
+            }}
+          />  
+        </Box>
       </div>
+      <div className="szurok">
+        <div className="szures">
+          <p>Filter:</p>
+          <span><IoFilter /> </span>
+        </div>
+        <div className="rendezes">
+          <p>Sort by:</p>
+          <span><BiSort /> </span>
+        </div>
+        
+      </div>
+    </div>
+
   )
 }
