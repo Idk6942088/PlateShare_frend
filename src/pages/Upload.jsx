3cd760aab917c2c27fe43f02a6ec4380256efcf2
Nundesz05 <nundesz06@gmail.com>
2025-02-03 23:00:59 +0100

    Database + login

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
-export default function Upload() {
+export default function Upload({partner}) {
   return (
-    <div>Upload</div>
+    <div>{partner ? "Upload" : <Navigate to="/"/>}</div>
   )
 }
