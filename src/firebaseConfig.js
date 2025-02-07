87d537e0ad555c4c7bf91c7e4cf6420353dfc9a2
aDyon4 <12adamdominik@gmail.com>
2025-02-07 14:50:08 +0100

    home background

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBLXeMoN9eagJTKbn5gZPZ8aHCU9Ii0bg8",
+    authDomain: "plateshare-f1fe1.firebaseapp.com",
+    projectId: "plateshare-f1fe1",
+    storageBucket: "plateshare-f1fe1.firebasestorage.app",
+    messagingSenderId: "94970944357",
+    appId: "1:94970944357:web:32506f21bc29b5a6f59f8c"
+  };
