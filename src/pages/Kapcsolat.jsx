f2da40c49c8557896577dfbc4c23b3961d3b51d4
aDyon4 <144908900+aDyon4@users.noreply.github.com>
2025-02-24 14:46:59 +0100

    Kapcsolat.jsx, Etel.jsx fix

--- src/pages/Kapcsolat.jsx
+++ src/pages/Kapcsolat.jsx
@@ -1,7 +1,32 @@
+import { TextField } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import React from 'react'
 
 export default function Kapcsolat() {
   return (
-    <div>Kapcsolat</div>
+    <div className='kapcsolat'>
+      <Grid container >
+        <Grid >
+          <div className="uzenetKuldes">
+            <h1>Üzenet küldés</h1>
+              <input type="text" placeholder="Neved" id="" />
+              <input type="text" placeholder="Email címed" id="" />
+              <input type="text" placeholder="Telefonszám" id="" />
+              <textarea name="" id=""></textarea>
+            
+          </div>
+        </Grid>
+        <Grid >
+          <div className="kapcsolatInfo">
+            <h1>Kapcsolat</h1>
+            <div className="kapcsolat_img">
+              <img src="src/img/instagram.png" alt="" />
+              <img src="src/img/facebook.png" alt="" />
+              <img src="src/img/tiktok.png" alt="" />
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
