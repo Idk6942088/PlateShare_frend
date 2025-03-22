e952f32dda8fd456e101768e663f08a7b79c30c8
Nundesz05 <nundesz06@gmail.com>
2025-03-22 22:53:08 +0100

    Admin page + kapcsolat

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,36 +1,109 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Paper } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { MdDeleteForever } from 'react-icons/md';
 import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom'
 
 export default function Admin({admin,db}) {
 
   const [users,setUsers] = useState([]);
+  const [messages,setMessages] = useState([]);
+  const [uzenet,setUzenet] = useState(null);
+  const [rowid,setRowid] = useState(null);
+  
+  let nextId=0;
 
   useEffect(() => {
-    if(users.length == 0) {
-      async function getUsers() {
-        const snap = await getDocs(collection(db, "users"));
-        const lst = snap.docs.map(doc => ({ ...doc.data(), id:doc.id }));
-      setUsers(lst);
-      }
-      getUsers();
+    async function getMessages() {
+      const snap = await getDocs(collection(db, "uzenetek"));
+      const lst = snap.docs.map(doc => ({ ...doc.data(), id:doc.id }));
+    setMessages(lst);
     }
+    getMessages();
 
-  },[]);
-  
-  
+},[]);
+
+useEffect(() => {
+  async function getUsers() {
+    let lst =[]
+    for(let a of messages) {
+      const snap = await getDoc(doc(db, "users", a.id));
+      if (snap.exists()){
+        lst.push(snap.data());
+        lst[nextId].nev=lst[nextId].veznev+" "+lst[nextId].kernev;
+        lst[nextId].id=nextId++;
+      }
+    }
+    setUsers(lst);
+  }
+  getUsers();
+
+},[messages]);
+
 
 const navigate = useNavigate();
   const location=useLocation()
-  console.log(location.pathname);
-  console.log(users);
   const userspage=location.pathname=='/admin/felhasznalok';
-  const messages=location.pathname=='/admin/uzenetek';
+  const messagespage=location.pathname=='/admin/uzenetek';
+  const messagepage=location.pathname=='/admin/uzenetek/uzenet';
   const orders=location.pathname=='/admin/rendelesek';
   const termekek=location.pathname=='/admin/termekek';
+  
 
+  const paginationModel = { page: 0, pageSize: 5 };
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = (e,row) => {
+    setOpen(true);
+    e.stopPropagation();
+    setRowid(row.id);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'nev', headerName: 'Név', width: 100 },
+    { field: 'tipus', headerName: 'Felhasználó típus', width: 130 },
+    { field: 'delete', headerName: '', width: 115, renderCell: (params) => {
+      return (
+        <>
+        <MdDeleteForever className='bg-red-500 m-auto p-1 my-2  text-white text-4xl rounded-md' onClick={(e) => handleClickOpen(e,params.row)}/>
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"FELHASZNÁLÓI FIÓK TÖRLÉSE"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Biztosan törölni szeretéd ezt a felhasználót?<br/>
+            {rowid!=null?users[rowid].nev:""}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Mégse</Button>
+          <Button onClick={handleClose} autoFocus>
+            Törlés
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </>
+      );
+    } },
+  ];
+  
   return (
     <div className='adminpage'>{admin ? 
       <div>
@@ -38,39 +111,28 @@ const navigate = useNavigate();
         <div className='adminmenu flex flex-col gap-5 justify-center mt-5 pt-5'>
           <div className='adminmenu_content flex flex-row gap-5 justify-center'>
           <Link to="/admin/felhasznalok">Felhasználók</Link>
-          <Link to="/admin/termek">Termékek</Link>
-          <Link to="/admin/rendelesek">Rendelések</Link>
           <Link to="/admin/uzenetek">Üzenetek</Link>
           </div>
           <div className='admincontent'>
             {userspage ? 
-              
-                <div className='userslist'>
-                
-                  <table>
-                  <tr>
-                  <td className='userheader'>Email</td>
-                  <td>Név</td>
-                  <td>Felhasználó típus</td>
-                  <td></td>
-                  </tr>
-                 
-                  
-                  {users.map(user => (
-                    <tr key={user.id} className='userrow'>
-                      <td className='useremail'>{user.email}</td>
-                      <td>{user.veznev} {user.kernev}</td>
-                      <td>{user.tipus}</td>
-                      <td className='usertorol'>X</td>
-                    </tr>
-                  ))}
-                  </table>
-                </div>
-              
+                <>
+                  <Paper sx={{ height: 350, width: '100%'}}>
+                  <DataGrid
+                    rows={users}
+                    columns={columns}
+                    initialState={{ pagination: { paginationModel } }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                    
+                    sx={{ border: 0 }}
+                  />
+                  </Paper>
+                  </>
             
             : ""}
-            {messages ? "Üzenetek" : ""}
-          
+            {messagespage && messages.length==0 ? "Nincsenek üzenetek" : 
+            <div className=''> {messagespage && users.map((x,i)=> <div className='uzenetbox'><span>{x.veznev} {x.kernev}</span> <span>{x.email}</span> {<Link to="/admin/uzenetek/uzenet"><Button variant="contained" onClick={()=>setUzenet(i)} >Megnyitás</Button></Link>} {<Button variant="contained" color='error'>Lezárás</Button>}</div>  )} </div>}
+            {messagepage ? <div className='uzenetcontent'>{messages.map((x,i)=> <div className='bg-blue-600 text-white font-medium  p-2 m-2 rounded-xl'>{x.uzenet}</div>)}<Button variant="contained">Válasz küldése</Button> </div> : ""}
           </div>
         </div>
        
--- src/pages/Kapcsolat.jsx
+++ src/pages/Kapcsolat.jsx
@@ -3,20 +3,44 @@ import Grid from '@mui/material/Grid2';
 import React from 'react'
 import { MdEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 
-export default function Kapcsolat() {
+export default function Kapcsolat({user,db}) {
+
+  const [useradat,setUseradat] = useState([]);
+  const [uzenet,setUzenet] = useState("");
+  const [uzenetdb,setUzenetdb] = useState([]);
+
+  useEffect(() => {
+    if(user!=null) {
+      async function getUser() {
+      const snap = await getDoc(doc(db, "users", user.uid));
+      if (snap.exists()) setUseradat(snap.data());
+      }
+      getUser();
+      
+    } 
+  },[user]);
+
+  async function kuldes() {
+    
+    const snap = await getDoc(doc(db, "uzenetek", user.uid));
+    if (snap.exists()) setUzenetdb(snap.data());
+    let uzenetid="uzenet"+uzenetdb.length;
+    await setDoc(doc(db, "uzenetek", user.uid), {uzenet:uzenet});
+  }
+
   return (
     <div className='kapcsolat'>
-      <Grid container spacing={2}>
+      {!user ? (<Grid container spacing={2}>
         <Grid size={{ xs: 12, sm: 6, md: 6 }} >
           <div className="uzenetKuldes">
             <h1>Üzenet küldés</h1>
-              <input type="text" placeholder="Neved" id="" />
-              <input type="text" placeholder="Email címed" id="" />
-              <input type="text" placeholder="Telefonszámod" id="" />
-              <textarea name="" id=""></textarea>
-              <Button variant="outlined">Küldés</Button>
+            
+              
           </div>
         </Grid>
         <Grid size={{ xs: 12, sm: 6, md: 6 }}>
@@ -35,7 +59,40 @@ export default function Kapcsolat() {
             </div>
           </div>
         </Grid>
-      </Grid>
+      </Grid>):(<Grid container spacing={2}>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }} >
+          <div className="uzenetKuldes">
+            <h1>Üzenet küldés</h1>
+            
+            {useradat.length==0 ? "":
+              <>
+              <input type="text" placeholder={useradat.veznev+" "+useradat.kernev}  disabled/>
+              <input type="text" placeholder={useradat.email} disabled/>
+              <textarea value={uzenet} onChange={e => setUzenet(e.target.value)}></textarea>
+              <Button variant="outlined" onClick={()=>kuldes()}>Küldés</Button> </>
+             }
+           
+              
+          </div>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+          <div className="kapcsolatInfo">
+            <h1>Kapcsolat</h1>
+            <p className='mt-5'>Elérhetőségeink:</p>
+            <ul>
+              <li><MdEmail />@plateshare@gmail.com</li>
+              <li><FaPhoneAlt /> +36 12 345 6789</li>
+            </ul>
+            <div className="kapcsolat_img">
+              <span>Kövess minket:</span>
+              <img src="src/img/instagram.png" alt="" />
+              <img src="src/img/facebook.png" alt="" />
+              <img src="src/img/tiktok.png" alt="" />
+            </div>
+          </div>
+        </Grid>
+      </Grid>)}
+      
     </div>
   )
 }
